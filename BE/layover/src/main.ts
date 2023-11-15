5757c2c862665edf5928b13cf33faf81e160c2f3
whipbaek <jibaek0513@gmail.com>
2023-11-15 23:38:19 +0900

    :recycle: Refactor: exception 전역 적용

--- BE/layover/src/main.ts
+++ BE/layover/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './custom-exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
