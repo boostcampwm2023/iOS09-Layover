9a6c3a715d83979adc96d8e4d5dfe2ec5be499e6
hw-ani <pcbmlh73@gmail.com>
2023-11-15 22:55:36 +0900

    :heavy_plus_sign: Feat: 패키지 추가에 따른 추가 import
    - httpmodule, membermodule, jwt, redis

--- BE/layover/src/app.module.ts
+++ BE/layover/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './database/database.module';
+import { OauthModule } from './oauth/oauth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  //imports: [DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, OauthModule],
   controllers: [AppController],
   providers: [AppService],
 })
