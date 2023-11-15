239cc7fe63f3a2a392974288e2792e4e8eb983bc
hw-ani <pcbmlh73@gmail.com>
2023-11-15 22:42:09 +0900

    :sparkles: Feat: jwt 생성 함수 구현
    - 우리 서비스의 access, refresh token 발급을 위한 jwt 함수 구현

---
+++ BE/layover/src/utils/jwtPayload.ts
@@ -0,0 +1,26 @@
+import {
+  ACCESS_TOKEN_EXP_IN_SECOND,
+  REFRESH_TOKEN_EXP_IN_SECOND,
+} from 'src/config';
+
+type tokenType = 'access' | 'refresh';
+
+export function getJwtPaylaod(token: tokenType, memberHash: string): any {
+  const issuedNumericDate = Math.floor(Date.now() / 1000); // 현재 시점을 numericDate 형식으로 저장, Date.now()는 밀리세컨드 단위라 세컨드 단위로 나타내기 위해 1000으로 나눔.
+  const expirationPeriod =
+    token === 'access'
+      ? ACCESS_TOKEN_EXP_IN_SECOND
+      : REFRESH_TOKEN_EXP_IN_SECOND;
+  const uuid = 1234;
+
+  return {
+    iss: 'https://layover-public-ip',
+    exp: issuedNumericDate + expirationPeriod,
+    sub: `Layover${token}Token`,
+    aud: 'https://layover-public-ip',
+    nbf: issuedNumericDate,
+    iat: issuedNumericDate,
+    jti: uuid,
+    memberHash: `${memberHash}`,
+  };
+}
