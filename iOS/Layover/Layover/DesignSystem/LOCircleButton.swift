e7ec546be9ddae4af10551dd85e62c8a3134f927
loinsir <a9327370@gmail.com>
2023-11-15 20:10:16 +0900

    :bento: Add: 버튼 애셋 추가

---
+++ iOS/Layover/Layover/DesignSystem/LOCircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  LOCircleButton.swift
+//  Layover
+//
+//  Created by 김인환 on 11/15/23.
+//  Copyright © 2023 CodeBomber. All rights reserved.
+//
+
+import UIKit
+
+class LOCircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
