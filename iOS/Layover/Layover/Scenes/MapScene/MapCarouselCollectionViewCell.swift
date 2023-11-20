16ab1688e30b858912f6f697afc3d6032facae56
kong <1018dbrud@gmail.com>
2023-11-21 03:16:12 +0900

    :lipstick: Update: 지도 캐러셀 UI 및 LoopingPlayerView 뷰 추가

--- iOS/Layover/Layover/Scenes/MapScene/MapCarouselCollectionViewCell.swift
+++ iOS/Layover/Layover/Scenes/MapScene/MapCarouselCollectionViewCell.swift
@@ -7,16 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class MapCarouselCollectionViewCell: UICollectionViewCell {
 
+    private let loopingPlayerView = LoopingPlayerView()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .white
+        setUI()
+        render()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setUI()
+        render()
+    }
+
+    override func prepareForReuse() {
+        render()
+    }
+
+    func configure(urlString: String) {
+        loopingPlayerView.prepareVideo(with: URL(string: urlString)!,
+                                       timeRange: CMTimeRange(start: .zero, duration: CMTime(value: 1800, timescale: 600)))
+        loopingPlayerView.player?.isMuted = true
+    }
+
+    func didMoveToCenter() {
+        loopingPlayerView.play()
+    }
+
+    func didMoveToSide() {
+        loopingPlayerView.pause()
+    }
+
+    private func setUI() {
+        backgroundColor = .background
+        contentView.addSubview(loopingPlayerView)
+        loopingPlayerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        loopingPlayerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+        loopingPlayerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+        loopingPlayerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        loopingPlayerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+
+    private func render() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
     }
 
 }
