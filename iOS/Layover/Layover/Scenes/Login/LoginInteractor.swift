860e536ba8eed48d18c76c058909774538905b40
Jiung <44396392+chopmozzi@users.noreply.github.com>
2023-11-20 18:34:06 +0900

    :sparkles: feat: LoginView Layout 구현
    하는 김에 VIP 틀만 잡음

--- iOS/Layover/Layover/Scenes/Login/LoginInteractor.swift
+++ iOS/Layover/Layover/Scenes/Login/LoginInteractor.swift
@@ -8,17 +8,13 @@
 import UIKit
 
 protocol LoginBusinessLogic {
-    func fetchFromLocalDataStore(with request: LoginModels.FetchFromLocalDataStore.Request)
-    func fetchFromRemoteDataStore(with request: LoginModels.FetchFromRemoteDataStore.Request)
-    func trackAnalytics(with request: LoginModels.TrackAnalytics.Request)
-    func performLogin(with request: LoginModels.PerformLogin.Request)
+    func performKakaoLogin(with request: LoginModels.PerformKakaoLogin.Request)
+    func performAppleLogin(with request: LoginModels.PerformAppleLogin.Request)
 }
 
-protocol LoginDataStore {
-    var exampleVariable: String? { get set }
-}
+protocol LoginDataStore { }
 
-class LoginInteractor: LoginBusinessLogic, LoginDataStore {
+final class LoginInteractor {
 
     // MARK: - Properties
 
@@ -27,59 +23,21 @@ class LoginInteractor: LoginBusinessLogic, LoginDataStore {
     lazy var worker = LoginWorker()
     var presenter: LoginPresentationLogic?
 
-    var exampleVariable: String?
+}
 
-    // MARK: - Use Case - Fetch From Local DataStore
+// MARK: - Use Case - Login
 
-    func fetchFromLocalDataStore(with request: LoginModels.FetchFromLocalDataStore.Request) {
-        let response = Models.FetchFromLocalDataStore.Response()
-        presenter?.presentFetchFromLocalDataStore(with: response)
+extension LoginInteractor: LoginBusinessLogic {
+    func performKakaoLogin(with request: LoginModels.PerformKakaoLogin.Request) {
+        // TODO: Logic 작성
     }
 
-    // MARK: - Use Case - Fetch From Remote DataStore
-
-    func fetchFromRemoteDataStore(with request: LoginModels.FetchFromRemoteDataStore.Request) {
-        // fetch something from backend and return the values here
-        // <#Network Worker Instance#>.fetchFromRemoteDataStore(completion: { [weak self] code in
-        //     let response = Models.FetchFromRemoteDataStore.Response(exampleVariable: code)
-        //     self?.presenter?.presentFetchFromRemoteDataStore(with: response)
-        // })
+    func performAppleLogin(with request: LoginModels.PerformAppleLogin.Request) {
+        // TODO: Logic 작성
     }
 
-    // MARK: - Use Case - Track Analytics
+}
 
-    func trackAnalytics(with request: LoginModels.TrackAnalytics.Request) {
-        // call analytics library/wrapper here to track analytics
-        // <#Analytics Worker Instance#>.trackAnalytics(event: request.event)
+extension LoginInteractor: LoginDataStore {
 
-        let response = Models.TrackAnalytics.Response()
-        presenter?.presentTrackAnalytics(with: response)
-    }
-
-    // MARK: - Use Case - Login
-
-    func performLogin(with request: LoginModels.PerformLogin.Request) {
-        let error = worker.validate(exampleVariable: request.exampleVariable)
-
-        if let error = error {
-            let response = Models.PerformLogin.Response(error: error)
-            presenter?.presentPerformLogin(with: response)
-            return
-        }
-
-        // <#Network Worker Instance#>.performLogin(completion: { [weak self, weak request] isSuccessful, error in
-        //     self?.completion(request?.exampleVariable, isSuccessful, error)
-        // })
-    }
-
-    private func completion(_ exampleVariable: String?, _ isSuccessful: Bool, _ error: Models.LoginError?) {
-        if isSuccessful {
-            // do something on success
-            let goodExample = exampleVariable ?? ""
-            self.exampleVariable = goodExample
-        }
-
-        let response = Models.PerformLogin.Response(error: error)
-        presenter?.presentPerformLogin(with: response)
-    }
 }
--- iOS/Layover/Layover/Scenes/Login/LoginViewController.swift
+++ iOS/Layover/Layover/Scenes/Login/LoginViewController.swift
@@ -6,149 +6,93 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-protocol LoginDisplayLogic: class {
-    func displayFetchFromLocalDataStore(with viewModel: LoginModels.FetchFromLocalDataStore.ViewModel)
-    func displayFetchFromRemoteDataStore(with viewModel: LoginModels.FetchFromRemoteDataStore.ViewModel)
-    func displayTrackAnalytics(with viewModel: LoginModels.TrackAnalytics.ViewModel)
-    func displayPerformLogin(with viewModel: LoginModels.PerformLogin.ViewModel)
+#Preview {
+    LoginViewController()
 }
 
-class LoginViewController: UIViewController, LoginDisplayLogic {
+protocol LoginDisplayLogic: AnyObject {
+    func displayPerformKakaoLogin(with viewModel: LoginModels.PerformKakaoLogin.ViewModel)
+    func displayPerformAppleLogin(with viewModel: LoginModels.PerformAppleLogin.ViewMdoel)
+}
+
+final class LoginViewController: BaseViewController {
 
     // MARK: - Properties
+    private let logoImage: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.image = UIImage.logo
+        return imageView
+    }()
+
+    private let logoTitleLabel: UILabel = {
+        let titleLabel: UILabel = UILabel()
+        titleLabel.font = .loFont(type: .logoTitle)
+        titleLabel.text = "Layover"
+        return titleLabel
+    }()
+
+    private let kakaoLoginButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.setBackgroundImage(UIImage.kakaologin, for: .normal)
+
+        return button
+    }()
+
+    private let appleLoginButton: UIControl = {
+        let appleLogin: UIControl = ASAuthorizationAppleIDButton(type: .continue, style: .white)
+        return appleLogin
+    }()
 
     typealias Models = LoginModels
-    var router: (NSObjectProtocol & LoginRoutingLogic & LoginDataPassing)?
+
     var interactor: LoginBusinessLogic?
 
-    // MARK: - Object lifecycle
-
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        setup()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
-    }
-
-    // MARK: - Setup
-
-    private func setup() {
-        let viewController = self
-        let interactor = LoginInteractor()
-        let presenter = LoginPresenter()
-        let router = LoginRouter()
-
-        viewController.router = router
-        viewController.interactor = interactor
-        interactor.presenter = presenter
-        presenter.viewController = viewController
-        router.viewController = viewController
-        router.dataStore = interactor
-    }
-
-    // MARK: - View Lifecycle
+    // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupFetchFromLocalDataStore()
+        LoginConfigurator.shared.configure(self)
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setupFetchFromRemoteDataStore()
-    }
+    // MARK: - UI + Layout
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        trackScreenViewAnalytics()
-        registerNotifications()
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        unregisterNotifications()
-    }
-
-    // MARK: - Notifications
-
-    func registerNotifications() {
-        let selector = #selector(trackScreenViewAnalytics)
-        let notification = UIApplication.didBecomeActiveNotification
-        NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: nil)
-    }
-
-    func unregisterNotifications() {
-        NotificationCenter.default.removeObserver(self)
-    }
-
-    // MARK: - Use Case - Fetch From Local DataStore
-
-    @IBOutlet var exampleLocalLabel: UILabel! = UILabel()
-    func setupFetchFromLocalDataStore() {
-        let request = Models.FetchFromLocalDataStore.Request()
-        interactor?.fetchFromLocalDataStore(with: request)
-    }
-
-    func displayFetchFromLocalDataStore(with viewModel: LoginModels.FetchFromLocalDataStore.ViewModel) {
-        exampleLocalLabel.text = viewModel.exampleTranslation
-    }
-
-    // MARK: - Use Case - Fetch From Remote DataStore
-
-    @IBOutlet var exampleRemoteLabel: UILabel! = UILabel()
-    func setupFetchFromRemoteDataStore() {
-        let request = Models.FetchFromRemoteDataStore.Request()
-        interactor?.fetchFromRemoteDataStore(with: request)
-    }
-
-    func displayFetchFromRemoteDataStore(with viewModel: LoginModels.FetchFromRemoteDataStore.ViewModel) {
-        exampleRemoteLabel.text = viewModel.exampleVariable
-    }
-
-    // MARK: - Use Case - Track Analytics
-
-    @objc
-    func trackScreenViewAnalytics() {
-        trackAnalytics(event: .screenView)
-    }
-
-    func trackAnalytics(event: LoginModels.AnalyticsEvents) {
-        let request = Models.TrackAnalytics.Request(event: event)
-        interactor?.trackAnalytics(with: request)
-    }
-
-    func displayTrackAnalytics(with viewModel: LoginModels.TrackAnalytics.ViewModel) {
-        // do something after tracking analytics (if needed)
-    }
-
-    // MARK: - Use Case - Login
-
-    func performLogin(_ sender: Any) {
-        let request = Models.PerformLogin.Request(exampleVariable: exampleLocalLabel.text)
-        interactor?.performLogin(with: request)
-    }
-
-    func displayPerformLogin(with viewModel: LoginModels.PerformLogin.ViewModel) {
-        // handle error and ui element error states
-        // based on error type
-        if let error = viewModel.error {
-            switch error.type {
-            case .emptyExampleVariable:
-                exampleLocalLabel.text = error.message
-
-            case .networkError:
-                exampleLocalLabel.text = error.message
-            }
-
-            return
+    override func setConstraints() {
+        view.addSubviews(logoImage, logoTitleLabel, kakaoLoginButton, appleLoginButton)
+        [logoImage, logoTitleLabel, kakaoLoginButton, appleLoginButton].forEach { subView in
+            subView.translatesAutoresizingMaskIntoConstraints = false
         }
 
-        // handle ui element success state and
-        // route to next screen
-        router?.routeToNext()
+        NSLayoutConstraint.activate([
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 280),
+            logoImage.widthAnchor.constraint(equalToConstant: 81.24),
+            logoImage.heightAnchor.constraint(equalToConstant: 58.12),
+            logoTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoTitleLabel.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 14.88),
+            kakaoLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            kakaoLoginButton.topAnchor.constraint(equalTo: logoTitleLabel.bottomAnchor, constant: 81),
+            kakaoLoginButton.widthAnchor.constraint(equalToConstant: 315),
+            kakaoLoginButton.heightAnchor.constraint(equalToConstant: 48),
+            appleLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appleLoginButton.topAnchor.constraint(equalTo: kakaoLoginButton.bottomAnchor, constant: 8),
+            appleLoginButton.widthAnchor.constraint(equalToConstant: 315),
+            appleLoginButton.heightAnchor.constraint(equalToConstant: 48)
+        ])
     }
+
+}
+
+// MARK: - Use Case - Login
+
+extension LoginViewController: LoginDisplayLogic {
+    func displayPerformKakaoLogin(with viewModel: LoginModels.PerformKakaoLogin.ViewModel) {
+        // TODO: Logic 작성
+    }
+
+    func displayPerformAppleLogin(with viewModel: LoginModels.PerformAppleLogin.ViewMdoel) {
+        // TODO: Logic 작성
+    }
+
 }
