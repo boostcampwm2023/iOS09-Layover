bbb128993853530d9e871225b6e06f8a4ffbfeaa
Jiung <44396392+chopmozzi@users.noreply.github.com>
2023-11-20 22:31:11 +0900

    :white_check_mark: chore: 리뷰 사항 반영

--- iOS/Layover/Layover/Scenes/Login/LoginViewController.swift
+++ iOS/Layover/Layover/Scenes/Login/LoginViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 import AuthenticationServices
 
-#Preview {
-    LoginViewController()
-}
-
 protocol LoginDisplayLogic: AnyObject {
     func displayPerformKakaoLogin(with viewModel: LoginModels.PerformKakaoLogin.ViewModel)
     func displayPerformAppleLogin(with viewModel: LoginModels.PerformAppleLogin.ViewMdoel)
@@ -49,10 +45,19 @@ final class LoginViewController: BaseViewController {
 
     var interactor: LoginBusinessLogic?
 
-    // MARK: - Life cycle
+    // MARK: - Object lifecycle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    // MARK: - Setup
+    private func setup() {
         LoginConfigurator.shared.configure(self)
     }
 
@@ -95,4 +100,8 @@ extension LoginViewController: LoginDisplayLogic {
         // TODO: Logic 작성
     }
 
+}
+
+#Preview {
+    LoginViewController()
 }
