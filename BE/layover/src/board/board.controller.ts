c01fa840692e632f994cf94345d2eae789dccfaf
whipbaek <jibaek0513@gmail.com>
2023-11-22 00:22:11 +0900

    :recycle: Refactor: signed url 서비스로 분리

--- BE/layover/src/board/board.controller.ts
+++ BE/layover/src/board/board.controller.ts
@@ -1,27 +1,33 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
+import { BoardService } from './board.service';
 @Controller('board')
 export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
   @Post('presigned-url')
   async getPresignedUrl(
     @Body('filename') filename: string,
     @Body('filetype') filetype: string,
   ) {
-    const s3 = new AWS.S3({
-      endpoint: process.env.NCLOUD_S3_ENDPOINT,
-      credentials: {
-        accessKeyId: process.env.NCLOUD_S3_ACCESS_KEY,
-        secretAccessKey: process.env.NCLOUD_S3_SECRET_KEY,
-      },
-      region: process.env.NCLOUD_S3_REGION,
-    });
-
-    const preSignedUrl: string = s3.getSignedUrl('putObject', {
-      Bucket: process.env.NCLOUD_S3_BUCKET_NAME,
-      Key: `${filename}.${filetype}`,
-      Expires: 60 * 60, // URL 만료되는 시간(초 단위)
-      ContentType: `video/${filetype}`,
-    });
+    const { preSignedUrl } = this.boardService.makePreSignedUrl(
+      filename,
+      filetype,
+    );
     return { preSignedUrl };
+  }
+
+  @Post()
+  async createBoard(
+    @Body('title') title: string,
+    @Body('content') content: string,
+    @Body('location') location: string,
+  ) {
+    await this.boardService.createBoard(title, content, location);
+
+    return {
+      title,
+      content,
+      location,
+    };
   }
 }
