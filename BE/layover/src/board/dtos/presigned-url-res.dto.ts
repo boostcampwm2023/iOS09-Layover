e65b2cc98d00016b22c26179af9e8a7f2028ede1
whipbaek <jibaek0513@gmail.com>
2023-11-23 02:52:05 +0900

    :truck: Chore: example 파트 수정

--- BE/layover/src/board/dtos/presigned-url-res.dto.ts
+++ BE/layover/src/board/dtos/presigned-url-res.dto.ts
@@ -2,7 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class PresignedUrlResDto {
   @ApiProperty({
-    example: 'someUrl',
+    example:
+      'https://layover-original-video.s3.amazonaws.com/14bb1a79093f5b9443e9b8105.....',
     description: '동영상을 업로드 하기 위해 요청할 링크',
   })
   readonly presignedUrl: string;
