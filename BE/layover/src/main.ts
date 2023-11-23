97983a51c755494b318d807c902b71e20f3bef77
whipbaek <jibaek0513@gmail.com>
2023-11-23 14:20:31 +0900

    :truck: Chore: validation pipe 적용

--- BE/layover/src/main.ts
+++ BE/layover/src/main.ts
@@ -6,6 +6,7 @@ import { GlobalExceptionFilter } from './response/custom-response';
 import { readFileSync } from 'fs';
 import { TokenResDto } from './oauth/dtos/token-res.dto';
 import { PresignedUrlResDto } from './board/dtos/presigned-url-res.dto';
+import { ValidationPipe } from '@nestjs/common';
 const httpsOptions = {
   key: readFileSync('./private.key'),
   cert: readFileSync('./certificate.crt'),
@@ -15,6 +16,13 @@ const httpsOptions = {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whitelist: true, // decorator 없는 property의 요청을 막아줍니다.
+      // forbidNonWhitelisted: true, // whitelist 설정에 위반되는 property가 요청에 포함되면 요청 자체를 막아줍니다.
+      transform: true, // 요청에서 받은 데이터를 DTO에서 정의한 타입으로 변환해줍니다.
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Layover API Documentation')
     .setDescription('The Layover API description')
