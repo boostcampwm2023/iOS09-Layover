3c2a4fd8fd75b658e7259b0bcd72ac41e633c3ef
Jiung <44396392+chopmozzi@users.noreply.github.com>
2023-11-24 00:39:22 +0900

    :bug: 커밋 잘못들어간거 수정

--- iOS/Layover/Layover/Scenes/Tabbar/MainTabBarConfigurator.swift
+++ iOS/Layover/Layover/Scenes/Tabbar/MainTabBarConfigurator.swift
@@ -16,7 +16,7 @@ final class MainTabBarConfigurator: Configurator {
     func configure(_ viewController: UITabBarController) {
         let homeViewController = UINavigationController(rootViewController: HomeViewController())
         let mapViewController = UINavigationController(rootViewController: MapViewController())
-        let profileViewController = PlaybackViewController()
+        let profileViewController = DummyViewController()
 
         let homeIconImage = UIImage(resource: .planet)
         let mapIconImage = UIImage(resource: .map)
