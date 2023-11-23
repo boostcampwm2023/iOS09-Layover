e5852078b504704a0386abfe63d31d5c2ca6f86e
hw-ani <pcbmlh73@gmail.com>
2023-11-23 16:30:19 +0900

    :truck: Chore: import 경로 수정

--- BE/layover/src/member/member.service.ts
+++ BE/layover/src/member/member.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Member } from './member.entity';
-import { MAX_USERNAME } from 'src/config';
+import { MAX_USERNAME } from '../config';
 
 @Injectable()
 export class MemberService {
