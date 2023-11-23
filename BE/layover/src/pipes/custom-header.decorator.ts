6d301d11b055022529735f4a32d15901b089f4d0
hw-ani <pcbmlh73@gmail.com>
2023-11-23 21:47:27 +0900

    :sparkles: Custom decorator 추가
    요청시 파이프에서 헤더에 접근하기 위해 custom decorator 추가해서 사용함

---
+++ BE/layover/src/pipes/custom-header.decorator.ts
@@ -0,0 +1,8 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const CustomHeader = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    return data ? req.headers[data] : req.headers;
+  },
+);
