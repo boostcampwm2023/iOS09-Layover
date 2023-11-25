a6df4f833f2faf18cd6929ee6a9eb947aa631567
loinsir <a9327370@gmail.com>
2023-11-26 02:00:15 +0900

    :sparkles: UserDefault 래퍼 구현, AuthManager에 로그인 여부 확인 값 추가

--- iOS/Layover/Layover/Services/UserDefaults/UserDefaultStored.swift
+++ iOS/Layover/Layover/Services/UserDefaults/UserDefaultStored.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct UserDefaultStored<T> {
+    private let key: String
+    private let defaultValue: T
+
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
+enum UserDefaultKey {
+    static let isLoggedIn = "isLoggedIn"
+}
