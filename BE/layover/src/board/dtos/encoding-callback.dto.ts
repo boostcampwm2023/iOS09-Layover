b430d8f2803d858287a3a8651d32c8d4277b5a48
whipbaek <jibaek0513@gmail.com>
2023-11-26 05:34:36 +0900

    ✨ callback dto 추가

---
+++ BE/layover/src/board/dtos/encoding-callback.dto.ts
@@ -0,0 +1,53 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class EncodingCallbackDto {
+  @ApiProperty({
+    example: 10628,
+    description: '인코딩 카테고리 ID',
+  })
+  @IsNumber()
+  readonly categoryId: number;
+
+  @ApiProperty({
+    example: 'callback-test',
+    description: '인코딩 카테고리 이름',
+  })
+  @IsString()
+  readonly categoryNames: string;
+
+  @ApiProperty({
+    example: 1,
+    description: '인코딩 옵션 ID',
+  })
+  @IsNumber()
+  readonly encodingOptionId: number;
+
+  @ApiProperty({
+    example: 10808,
+    description: '입력 파일 ID',
+  })
+  @IsNumber()
+  readonly fileId: number;
+
+  @ApiProperty({
+    example: '/.../Guide720_AVC_HD_1Pass_30fps.mp4',
+    description: '입력 파일 경로',
+  })
+  @IsString()
+  readonly filePath: string;
+
+  @ApiProperty({
+    example: 'AVC_HD_1Pass_30fps',
+    description: '출력 인코딩 옵션 이름',
+  })
+  @IsString()
+  readonly outputType: string;
+
+  @ApiProperty({
+    example: 'RUNNING',
+    description: '입력 파일의 인코딩 상태',
+  })
+  @IsString()
+  readonly status: string;
+}
