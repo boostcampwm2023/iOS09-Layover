4a4f19d05733b339eae6021711dd3d3e39d8c955
chopmozzi <44396392+chopmozzi@users.noreply.github.com>
2023-11-28 17:25:50 +0900

    :sparkles: VideoDTO 생성

---
+++ iOS/Layover/Layover/Network/DTOs/VideoDTO.swift
@@ -0,0 +1,19 @@
+//
+//  VideoDTO.swift
+//  Layover
+//
+//  Created by 황지웅 on 11/28/23.
+//  Copyright © 2023 CodeBomber. All rights reserved.
+//
+
+import Foundation
+
+struct VideoDTO: Codable {
+    let title: String
+    let content: String
+    let location: String
+    let tags: [String]
+    // TODO: 프로필 완성되면 변경
+    let profile: String
+    let videoURLs: [URL]
+}
