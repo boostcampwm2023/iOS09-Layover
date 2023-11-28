5655bb0c47099fea903589184f7ed4d9ed82b76a
whipbaek <jibaek0513@gmail.com>
2023-11-29 02:41:29 +0900

    :bug:  의존성 에러 수정

--- BE/layover/src/tag/tag.module.ts
+++ BE/layover/src/tag/tag.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { tagProvider } from './tag.provider';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [...tagProvider, TagService],
+  exports: [TagService],
 })
 export class TagModule {}
