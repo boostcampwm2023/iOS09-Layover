f34455b9b5b09c9a1b3da39019071a00dfaa394d
chopmozzi <44396392+chopmozzi@users.noreply.github.com>
2023-11-30 14:24:19 +0900

    :sparkles: 설정 뷰 UI 구현

--- iOS/Layover/Layover/Scenes/SettingScene/SettingSceneViewController.swift
+++ iOS/Layover/Layover/Scenes/SettingScene/SettingSceneViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 protocol SettingSceneDisplayLogic: AnyObject {
 }
 
-final class SettingSceneViewController: BaseViewController, SettingSceneDisplayLogic {
+final class SettingSceneViewController: BaseViewController {
 
     // MARK: - Properties
 
@@ -19,6 +19,15 @@ final class SettingSceneViewController: BaseViewController, SettingSceneDisplayL
     var router: (NSObjectProtocol & SettingSceneRoutingLogic & SettingSceneDataPassing)?
     var interactor: SettingSceneBusinessLogic?
 
+    private let settingSceneTableView: UITableView = {
+        let tableView: UITableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    let policy: [String] = ["이용약관", "개인정보 처리 방침", "위치정보 이용 약관"]
+    let versionInfo: [String] = ["버전정보"]
+    let userInfo: [String] = ["로그아웃", "탈퇴"]
     // MARK: - Object lifecycle
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -51,7 +60,79 @@ final class SettingSceneViewController: BaseViewController, SettingSceneDisplayL
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        settingSceneTableView.delegate = self
+        settingSceneTableView.dataSource = self
+        settingSceneTableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.identifier)
     }
 
+    override func setConstraints() {
+        view.addSubview(settingSceneTableView)
+        NSLayoutConstraint.activate([
+            settingSceneTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            settingSceneTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            settingSceneTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            settingSceneTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+}
+
+extension SettingSceneViewController: SettingSceneDisplayLogic {
+
+}
+
+extension SettingSceneViewController: UITableViewDelegate {
+
+}
+
+extension SettingSceneViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 3
+        case 1:
+            return 1
+        case 2:
+            return 2
+        default:
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "약관 및 정책"
+        default:
+            return nil
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.identifier, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        switch indexPath.section {
+        case 0:
+            content.text = policy[indexPath.row]
+            cell.contentConfiguration = content
+        case 1:
+            content.text = versionInfo[indexPath.row]
+            content.secondaryText = "1.0.0"
+            cell.contentConfiguration = content
+        case 2:
+            content.text = userInfo[indexPath.row]
+            cell.contentConfiguration = content
+        default:
+            break
+        }
+        return cell
+    }
+}
+
+#Preview {
+    SettingSceneViewController()
 }
