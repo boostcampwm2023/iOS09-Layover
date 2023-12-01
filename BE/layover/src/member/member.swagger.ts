106ef4986ea554c4a1b45647897dbbefb7990377
hw-ani <pcbmlh73@gmail.com>
2023-12-01 19:19:48 +0900

    :wrench: Member 응답 성공 스웨거 설정

---
+++ BE/layover/src/member/member.swagger.ts
@@ -0,0 +1,108 @@
+import { HttpStatus } from '@nestjs/common';
+import { getSchemaPath } from '@nestjs/swagger';
+import { CheckUsernameResDto } from './dtos/check-username-res.dto';
+import { UsernameResDto } from './dtos/username-res.dto';
+import { IntroduceResDto } from './dtos/introduce-res.dto';
+import { DeleteMemberResDto } from './dtos/delete-member-res.dto';
+import { ProfilePresignedUrlResDto } from './dtos/profile-presigned-url-res.dto';
+import { MemberInfosResDto } from './dtos/member-infos-res.dto';
+
+export const MEMBER_SWAGGER = {
+  CHECK_USER_NAME_SUCCESS: {
+    status: HttpStatus.OK,
+    description: '닉네임 검증 결과',
+    schema: {
+      type: 'object',
+      properties: {
+        customCode: { type: 'string', example: 'SUCCESS' },
+        message: { type: 'boolean', example: '성공' },
+        statusCode: { type: 'number', example: HttpStatus.OK },
+        data: { $ref: getSchemaPath(CheckUsernameResDto) },
+      },
+    },
+  },
+
+  UPDATE_USER_NAME_SUCCESS: {
+    status: HttpStatus.OK,
+    description: '닉네임 수정 결과',
+    schema: {
+      type: 'object',
+      properties: {
+        customCode: { type: 'string', example: 'SUCCESS' },
+        message: { type: 'boolean', example: '성공' },
+        statusCode: { type: 'number', example: HttpStatus.OK },
+        data: { $ref: getSchemaPath(UsernameResDto) },
+      },
+    },
+  },
+
+  UPDATE_INTRODUCE_SUCCESS: {
+    status: HttpStatus.OK,
+    description: '자기소개 수정 결과',
+    schema: {
+      type: 'object',
+      properties: {
+        customCode: { type: 'string', example: 'SUCCESS' },
+        message: { type: 'boolean', example: '성공' },
+        statusCode: { type: 'number', example: HttpStatus.OK },
+        data: { $ref: getSchemaPath(IntroduceResDto) },
+      },
+    },
+  },
+
+  DELETE_MEMBER_SUCCESS: {
+    status: HttpStatus.OK,
+    description: '삭제된 회원 정보(닉네임)',
+    schema: {
+      type: 'object',
+      properties: {
+        customCode: { type: 'string', example: 'SUCCESS' },
+        message: { type: 'boolean', example: '성공' },
+        statusCode: { type: 'number', example: HttpStatus.OK },
+        data: { $ref: getSchemaPath(DeleteMemberResDto) },
+      },
+    },
+  },
+
+  GET_UPLOAD_PROFILE_PRESIGNED_URL_SUCCESS: {
+    status: HttpStatus.OK,
+    description: '프로필 이미지 업로드용 presigned url',
+    schema: {
+      type: 'object',
+      properties: {
+        customCode: { type: 'string', example: 'SUCCESS' },
+        message: { type: 'boolean', example: '성공' },
+        statusCode: { type: 'number', example: HttpStatus.OK },
+        data: { $ref: getSchemaPath(ProfilePresignedUrlResDto) },
+      },
+    },
+  },
+
+  GET_MEMBER_INFOS_SUCCESS: {
+    status: HttpStatus.OK,
+    description: '회원(본인) 정보들',
+    schema: {
+      type: 'object',
+      properties: {
+        customCode: { type: 'string', example: 'SUCCESS' },
+        message: { type: 'boolean', example: '성공' },
+        statusCode: { type: 'number', example: HttpStatus.OK },
+        data: { $ref: getSchemaPath(MemberInfosResDto) },
+      },
+    },
+  },
+
+  GET_OTHER_MEMBER_INFOS_SUCCESS: {
+    status: HttpStatus.OK,
+    description: '회원(타인) 정보들',
+    schema: {
+      type: 'object',
+      properties: {
+        customCode: { type: 'string', example: 'SUCCESS' },
+        message: { type: 'boolean', example: '성공' },
+        statusCode: { type: 'number', example: HttpStatus.OK },
+        data: { $ref: getSchemaPath(MemberInfosResDto) },
+      },
+    },
+  },
+};
