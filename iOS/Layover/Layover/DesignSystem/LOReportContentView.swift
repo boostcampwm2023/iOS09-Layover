b65f03ac10ceee6ca360f1019685153f893a581d
chopmozzi <44396392+chopmozzi@users.noreply.github.com>
2023-12-05 02:06:52 +0900

    :sparkles: radio button ui 구성

--- iOS/Layover/Layover/DesignSystem/LOReportContentView.swift
+++ iOS/Layover/Layover/DesignSystem/LOReportContentView.swift
@@ -12,26 +12,30 @@ final class LOReportContentView: UIView {
 
     // MARK: - UI Components
 
+    var index: Int = 0
+    
     private let whiteCircle: UIView = {
         let view: UIView = UIView()
         view.backgroundColor = .layoverWhite
         view.clipsToBounds = true
         view.layer.cornerRadius = 4
+        view.isHidden = true
+        view.isUserInteractionEnabled = false
         return view
     }()
 
-    private let radioButton: UIButton = {
-        let button: UIButton = UIButton()
-        button.layer.borderColor = UIColor.grey100.cgColor
-        button.layer.borderWidth = 0.1
-        button.backgroundColor = .clear
-        button.layer.cornerRadius = 10
-        button.clipsToBounds = true
-        button.backgroundColor = .primaryPurple
-        return button
+    private let radioView: UIView = {
+        let view: UIView = UIView()
+        view.layer.borderColor = UIColor.grey100.cgColor
+        view.layer.borderWidth = 0.1
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        view.isUserInteractionEnabled = false
+        return view
     }()
 
-    private let contentLabel: UILabel = {
+    let contentLabel: UILabel = {
         let label: UILabel = UILabel()
         label.text = "스팸 / 홍보 도배글이에요"
         label.font = .loFont(type: .body2)
@@ -50,6 +54,16 @@ final class LOReportContentView: UIView {
         setConstraints()
     }
 
+    func onRadio() {
+        whiteCircle.isHidden = false
+        radioView.backgroundColor = .primaryPurple
+    }
+
+    func offRadio() {
+        whiteCircle.isHidden = true
+        radioView.backgroundColor = .clear
+    }
+
     func setText(_ content: String) {
         contentLabel.text = content
     }
@@ -57,22 +71,22 @@ final class LOReportContentView: UIView {
     // MARK: - UI Methods
 
     private func setConstraints() {
-        radioButton.addSubview(whiteCircle)
+        radioView.addSubview(whiteCircle)
         whiteCircle.translatesAutoresizingMaskIntoConstraints = false
-        addSubviews(radioButton, contentLabel)
+        addSubviews(radioView, contentLabel)
         subviews.forEach { subView in
             subView.translatesAutoresizingMaskIntoConstraints = false
         }
         NSLayoutConstraint.activate([
-            whiteCircle.centerXAnchor.constraint(equalTo: radioButton.centerXAnchor),
-            whiteCircle.centerYAnchor.constraint(equalTo: radioButton.centerYAnchor),
+            whiteCircle.centerXAnchor.constraint(equalTo: radioView.centerXAnchor),
+            whiteCircle.centerYAnchor.constraint(equalTo: radioView.centerYAnchor),
             whiteCircle.widthAnchor.constraint(equalToConstant: 8),
             whiteCircle.heightAnchor.constraint(equalToConstant: 8),
-            radioButton.widthAnchor.constraint(equalToConstant: 20),
-            radioButton.heightAnchor.constraint(equalToConstant: 20),
-            radioButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-            radioButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            radioButton.trailingAnchor.constraint(equalTo: contentLabel.leadingAnchor, constant: -12),
+            radioView.widthAnchor.constraint(equalToConstant: 20),
+            radioView.heightAnchor.constraint(equalToConstant: 20),
+            radioView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            radioView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            radioView.trailingAnchor.constraint(equalTo: contentLabel.leadingAnchor, constant: -12),
             contentLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
     }
--- iOS/Layover/Layover/DesignSystem/LOReportStackView.swift
+++ iOS/Layover/Layover/DesignSystem/LOReportStackView.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 final class LOReportStackView: UIStackView {
+
+    var reportViews: [LOReportContentView] = {
+        let contentArray: [String] = ["스팸 홍보 / 도배글이에요", "부적절한 사진이에요", "청소년에게 유해한 내용이에요", "욕설 / 혐오 / 차별적 표현을 담고있어요", "거짓 정보를 담고있어요", "기타"]
+        return contentArray.enumerated().map { index, content in
+            let loReportContentView: LOReportContentView = LOReportContentView()
+            loReportContentView.setText(content)
+            loReportContentView.index = index
+            return loReportContentView
+        }
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUI()
@@ -26,14 +37,26 @@ final class LOReportStackView: UIStackView {
         self.distribution = .fillProportionally
         self.axis = .vertical
         self.spacing = 8
+        guard let firstReportView: LOReportContentView = reportViews.first else { return }
+        firstReportView.onRadio()
     }
 
     private func addContent() {
-        let contentArray: [String] = ["스팸 홍보 / 도배글이에요", "부적절한 사진이에요", "청소년에게 유해한 내용이에요", "욕설 / 혐오 / 차별적 표현을 담고있어요", "거짓 정보를 담고있어요", "기타"]
-        contentArray.forEach { content in
-            let loReportContentView: LOReportContentView = LOReportContentView()
-            loReportContentView.setText(content)
-            addArrangedSubview(loReportContentView)
+        reportViews.forEach { reportView in
+            let addGesutre: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(reportViewDidTap(_:)))
+            reportView.addGestureRecognizer(addGesutre)
+            addArrangedSubview(reportView)
+        }
+    }
+
+    @objc private func reportViewDidTap(_ sender: UITapGestureRecognizer) {
+        guard let tempView: LOReportContentView = sender.view as? LOReportContentView else { return }
+        self.reportViews.forEach { reportView in
+            if reportView.index == tempView.index {
+                reportView.onRadio()
+            } else {
+                reportView.offRadio()
+            }
         }
     }
 }
