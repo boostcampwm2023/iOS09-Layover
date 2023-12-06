f94cfdede48e1245fb0a8cfd7fdb716c230b0902
kong <1018dbrud@gmail.com>
2023-12-06 19:11:29 +0900

    ✨ VideoPickerManager 추가

---
+++ iOS/Layover/Layover/Scenes/UploadPost/VideoPickerManager.swift
@@ -0,0 +1,50 @@
+//
+//  VideoPickerManager.swift
+//  Layover
+//
+//  Created by kong on 2023/12/06.
+//  Copyright © 2023 CodeBomber. All rights reserved.
+//
+
+import PhotosUI
+
+protocol VideoPickerDelegate: AnyObject {
+    func didFinishPickingVideo(_ url: URL)
+}
+
+final class VideoPickerManager: NSObject, PHPickerViewControllerDelegate {
+
+    weak var videoPickerDelegate: VideoPickerDelegate?
+
+    lazy var phPickerViewController: PHPickerViewController = {
+        var configuration = PHPickerConfiguration()
+        configuration.preferredAssetRepresentationMode = .current
+        configuration.filter = .videos
+        configuration.selectionLimit = 1
+        let phPickerViewController = PHPickerViewController(configuration: configuration)
+        phPickerViewController.delegate = self
+        return phPickerViewController
+    }()
+
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        guard let result = results.first else {
+            self.phPickerViewController.dismiss(animated: true)
+            return
+        }
+
+        _ = result.itemProvider.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier) { url, error in
+            if error != nil {
+                Task {
+                    await MainActor.run {
+                        Toast.shared.showToast(message: "지원하지 않는 동영상 형식입니다 T.T")
+                    }
+                }
+            }
+
+            if let url {
+                self.videoPickerDelegate?.didFinishPickingVideo(url)
+            }
+        }
+    }
+
+}
