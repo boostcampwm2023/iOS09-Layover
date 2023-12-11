56b368dd25572469c9ed4dfeb86f8dc29acab5f0
loinsir <a9327370@gmail.com>
2023-12-12 02:12:04 +0900

    :white_check_mark: 합칠 수 있는 테스트 케이스 합침

--- iOS/Layover/LayoverTests/Scenes/TagPlayList/TagPlayListInteractorTests.swift
+++ iOS/Layover/LayoverTests/Scenes/TagPlayList/TagPlayListInteractorTests.swift
@@ -76,7 +76,7 @@ final class TagPlayListInteractorTests: XCTestCase {
 
     // MARK: - Tests
 
-    func test_setTitleTag를_호출하면_presenter의_presentTitleTag를_호출한다() {
+    func test_setTitleTag를_호출하면_presenter의_presentTitleTag를_호출하고_자신의_titleTag를_전달한다() {
         // Arrange
         let spy = TagPlayListPresentationLogicSpy()
         sut.presenter = spy
@@ -91,21 +91,7 @@ final class TagPlayListInteractorTests: XCTestCase {
         XCTAssertEqual(sut.titleTag, spy.presentTitleTagResponse.titleTag, "setTitleTag()가 presenter에게 자신의 titleTag를 전달했다.")
     }
 
-    func test_setTitleTag를_호출하면_presenter에게_자신의_titleTag를_전달한다() {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        sut.titleTag = "testTitleTag"
-        let request = Models.FetchTitleTag.Request()
-
-        // Act
-        sut.setTitleTag(request: request)
-
-        // Assert
-        XCTAssertEqual(sut.titleTag, spy.presentTitleTagResponse.titleTag, "setTitleTag()가 presenter에게 자신의 titleTag를 전달했다.")
-    }
-
-    func test_fetchPlayList를_호출하면_presenter의_presentPlayList를_호출한다() async {
+    func test_fetchPlayList를_호출하면_자신의_posts에_데이터를_저장하고_presenter의_presentPlayList를_호출하여_올바른_갯수의_데이터를_전달한다() async {
         // Arrange
         let spy = TagPlayListPresentationLogicSpy()
         sut.presenter = spy
@@ -116,32 +102,8 @@ final class TagPlayListInteractorTests: XCTestCase {
 
         // Assert
         XCTAssertTrue(spy.presentPlayListCalled, "fetchPlayList()가 presenter의 presentPlayList()를 호출했다.")
-    }
-
-    func test_fetchPlayList를_호출하면_presenter의_presentPlayList에_올바른_갯수의_Posts_데이터를_전달한다() async {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        let request = Models.FetchPosts.Request()
-
-        // Act
-        _ = await sut.fetchPlayList(request: request).value
-
-        // Assert
-        XCTAssertEqual(spy.presentPlayListResponse.post.count, 4, "fetchPlayList()가 presenter에게 올바른 갯수의 posts 데이터를 전달했다.")
-    }
-
-    func test_fetchPlayList를_호출하면_자신의_posts에_올바른_갯수의_Posts_데이터를_저장한다() async {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        let request = Models.FetchPosts.Request()
-
-        // Act
-        _ = await sut.fetchPlayList(request: request).value
-
-        // Assert
         XCTAssertEqual(sut.posts.count, 4, "fetchPlayList()가 자신의 posts에 올바른 갯수의 posts 데이터를 저장했다.")
+        XCTAssertEqual(spy.presentPlayListResponse.post.count, 4, "fetchPlayList()가 presenter에게 올바른 갯수의 displayedPosts 데이터를 전달했다.")
     }
 
     func test_fetchPlayList를_호출하면_기존에_저장된_posts를_비우고_새로운_Posts_데이터를_저장한다() async {
@@ -158,11 +120,11 @@ final class TagPlayListInteractorTests: XCTestCase {
             return
         }
 
-        let dummyPost = Post(member: Member(identifier: 1,
+        let dummyPost = Post(member: Member(identifier: -1,
                                        username: "안유진",
                                        introduce: "난 아이브의 리더~",
                                        profileImageURL: dummyImageURL),
-                        board: Board(identifier: 1,
+                        board: Board(identifier: -1,
                                      title: "IZONE",
                                      description: "아이즈원",
                                      thumbnailImageURL: dummyImageURL,
@@ -171,30 +133,31 @@ final class TagPlayListInteractorTests: XCTestCase {
                                      longitude: 0.1029382),
                         tag: ["유진", "아이브", "IVE",])
 
-        sut.posts = [dummyPost, dummyPost]
+        sut.posts = [dummyPost]
         let request = Models.FetchPosts.Request()
 
         // Act
         _ = await sut.fetchPlayList(request: request).value
 
         // Assert
+        XCTAssert(!sut.posts.contains(where: {
+            $0.board.identifier == dummyPost.board.identifier
+            && $0.board.title == dummyPost.board.title
+            && $0.board.description == dummyPost.board.description
+            && $0.board.thumbnailImageURL == dummyPost.board.thumbnailImageURL
+            && $0.board.videoURL == dummyPost.board.videoURL
+            && $0.board.latitude == dummyPost.board.latitude
+            && $0.board.longitude == dummyPost.board.longitude
+            && $0.member.identifier == dummyPost.member.identifier
+            && $0.member.username == dummyPost.member.username
+            && $0.member.introduce == dummyPost.member.introduce
+            && $0.member.profileImageURL == dummyPost.member.profileImageURL
+            && $0.tag == dummyPost.tag
+        }))
         XCTAssertEqual(sut.posts.count, 4, "fetchPlayList()가 자신의 posts를 비우고, 새로운 posts 데이터를 저장했다.")
     }
 
-    func test_fetchMorePlayList를_호출하면_presenter의_presentMorePlayList를_호출한다() async {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        let request = Models.FetchMorePosts.Request()
-
-        // Act
-        _ = await sut.fetchMorePlayList(request: request).value
-
-        // Assert
-        XCTAssertTrue(spy.presentMorePlayListCalled, "fetchPlayMoreList()가 presenter의 presentMorePlayList()를 호출했다.")
-    }
-
-    func test_fetchPlayList_호출_이후_fetchMorePlayList를_호출하면_presenter에게_올바른_갯수의_Posts_데이터를_전달한다() async {
+    func test_fetchMorePlayList를_호출하면_자신의_posts에_데이터를_저장하고_presenter의_presentMorePlayList를_호출하여_올바른_갯수의_Posts_데이터를_전달한다() async {
         // Arrange
         let spy = TagPlayListPresentationLogicSpy()
         sut.presenter = spy
@@ -202,59 +165,31 @@ final class TagPlayListInteractorTests: XCTestCase {
         let fetchMoreRequest = Models.FetchMorePosts.Request()
 
         // Act
-        _ = await sut.fetchPlayList(request: fetchRequest).value
         _ = await sut.fetchMorePlayList(request: fetchMoreRequest).value
 
         // Assert
+        XCTAssertTrue(spy.presentMorePlayListCalled, "fetchPlayMoreList()가 presenter의 presentMorePlayList()를 호출했다.")
         XCTAssertEqual(spy.presentMorePlayListResponse.post.count, 4, "fetchPlayMoreList()가 presenter에게 올바른 갯수의 posts 데이터를 전달했다.")
     }
 
-    func test_fetchPlayList_호출_이후_fetchMorePlayList를_호출하면_자신의_posts에_올바른_갯수의_Posts_데이터를_저장한다() async {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        let fetchRequest = Models.FetchPosts.Request()
-        let request = Models.FetchMorePosts.Request()
-
-        // Act
-        _ = await sut.fetchPlayList(request: fetchRequest).value
-        _ = await sut.fetchMorePlayList(request: request).value
-
-        // Assert
-        XCTAssertEqual(sut.posts.count, 8, "fetchPlayMoreList()가 자신의 posts에 올바른 갯수의 posts 데이터를 저장했다.")
-    }
-
-    func test_fetchMorePlayList를_호출하다가_더이상_가져온_데이터가_없으면_본인의_post개수를_유지한다() async {
+    func test_fetchMorePlayList를_호출하다가_더이상_가져온_데이터가_없으면_본인의_post개수를_유지하고_presenter의_presentMorePlayList를_호출하여_빈_배열을_전달한다() async {
         // Arrange
         let spy = TagPlayListPresentationLogicSpy()
         sut.presenter = spy
         let request = Models.FetchMorePosts.Request()
 
-        // Act
+        // Act -> 3번 호출 이후 빈 배열을 반환하도록 Mocking됨
         _ = await sut.fetchMorePlayList(request: request).value
         _ = await sut.fetchMorePlayList(request: request).value
         _ = await sut.fetchMorePlayList(request: request).value
 
         // Assert
+        XCTAssertTrue(spy.presentMorePlayListCalled, "fetchPlayMoreList()가 presenter의 presentMorePlayList()를 호출했다.")
         XCTAssertEqual(sut.posts.count, 8, "fetchPlayMoreList()를 호출하다가 더이상 데이터가 없을 때 자신의 posts를 유지했다.")
-    }
-
-    func test_fetchMorePlayList를_호출하다가_더이상_데이터가_없으면_presenter에게_빈_배열을_전달한다() async {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        let request = Models.FetchMorePosts.Request()
-
-        // Act
-        _ = await sut.fetchMorePlayList(request: request).value
-        _ = await sut.fetchMorePlayList(request: request).value
-        _ = await sut.fetchMorePlayList(request: request).value
-
-        // Assert
         XCTAssertEqual(spy.presentMorePlayListResponse.post.count, 0, "fetchPlayMoreList()가 presenter에게 빈 배열을 전달했다.")
     }
 
-    func test_fetchMorePlayList를_호출하면_기존_자신의_posts에_새로운_Posts_데이터를_추가하여_저장한다() async {
+    func test_fetchMorePlayList를_호출하면_기존_자신의_posts에_새로운_Posts_데이터가_추가되어_저장한다() async {
         // Arrange
         let spy = TagPlayListPresentationLogicSpy()
         sut.presenter = spy
@@ -289,41 +224,25 @@ final class TagPlayListInteractorTests: XCTestCase {
         _ = await sut.fetchMorePlayList(request: request).value
 
         // Assert
+        XCTAssertTrue(spy.presentMorePlayListCalled, "fetchPlayMoreList()가 presenter의 presentMorePlayList()를 호출했다.")
+        XCTAssertEqual(sut.posts.filter({
+            $0.board.identifier == dummyPost.board.identifier
+            && $0.board.title == dummyPost.board.title
+            && $0.board.description == dummyPost.board.description
+            && $0.board.thumbnailImageURL == dummyPost.board.thumbnailImageURL
+            && $0.board.videoURL == dummyPost.board.videoURL
+            && $0.board.latitude == dummyPost.board.latitude
+            && $0.board.longitude == dummyPost.board.longitude
+            && $0.member.identifier == dummyPost.member.identifier
+            && $0.member.username == dummyPost.member.username
+            && $0.member.introduce == dummyPost.member.introduce
+            && $0.member.profileImageURL == dummyPost.member.profileImageURL
+            && $0.tag == dummyPost.tag
+        }).count, 4)
         XCTAssertEqual(sut.posts.count, 8, "fetchPlayMoreList()가 자신의 posts에 새로운 posts 데이터를 더해서 저장했다.")
     }
 
-    func test_fetchMorePlayList를_호출하다가_다시_fetchPlayList를_호출하면_자신의_posts를_초기화한다() async {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        let fetchRequest = Models.FetchPosts.Request()
-        let fetchMoreRequest = Models.FetchMorePosts.Request()
-
-        // Act
-        _ = await sut.fetchPlayList(request: fetchRequest).value
-        _ = await sut.fetchMorePlayList(request: fetchMoreRequest).value
-        _ = await sut.fetchMorePlayList(request: fetchMoreRequest).value
-        _ = await sut.fetchMorePlayList(request: fetchMoreRequest).value
-        _ = await sut.fetchPlayList(request: fetchRequest).value
-
-        // Assert
-        XCTAssertEqual(sut.posts.count, 4, "fetchPlayList()가 자신의 posts를 초기화했다.")
-    }
-
-    func test_showPostDetail을_호출하면_presenter의_presentPostDetail을_호출한다() {
-        // Arrange
-        let spy = TagPlayListPresentationLogicSpy()
-        sut.presenter = spy
-        let request = Models.ShowPostsDetail.Request(startIndex: 0)
-
-        // Act
-        sut.showPostsDetail(request: request)
-
-        // Assert
-        XCTAssertTrue(spy.presentPostsDetailCalled, "showPostDetail()가 presenter의 presentPostDetail()를 호출했다.")
-    }
-
-    func test_showPostDetail을_호출하면_자신의_postPlayStartIndex값을_올바르게_저장한다() {
+    func test_showPostDetail을_호출하면_자신의_postPlayStartIndex값을_올바르게_저장하고_presenter의_presentPostDetail을_호출한다() {
         // Arrange
         let spy = TagPlayListPresentationLogicSpy()
         sut.presenter = spy
@@ -334,5 +253,6 @@ final class TagPlayListInteractorTests: XCTestCase {
 
         // Assert
         XCTAssertEqual(sut.postPlayStartIndex, 7, "showPostDetail()가 presenter에게 올바른 startIndex를 전달했다.")
+        XCTAssertTrue(spy.presentPostsDetailCalled, "showPostDetail()가 presenter의 presentPostDetail()를 호출했다.")
     }
 }
