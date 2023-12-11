238b7b40566a8dc984b1892782b5f0eb9c553768
loinsir <a9327370@gmail.com>
2023-12-12 04:18:42 +0900

    :white_check_mark: HomePresenterTests 실제 값 비교하도록 테스트 로직 수정

--- iOS/Layover/LayoverTests/Seeds.swift
+++ iOS/Layover/LayoverTests/Seeds.swift
@@ -12,6 +12,32 @@ class Seeds {
     static let sampleImageData = try! Data(contentsOf: Bundle(for: Seeds.self).url(forResource: "sample", withExtension: "jpeg")!)
 
     struct Posts {
+        static let thumbnailImageNilPost = Post(member: Member(identifier: 1,
+                                        username: "안유진",
+                                        introduce: "안녕하세요, 아이브의 안유진입니다.",
+                                        profileImageURL: URL(string: "https://cdn.footballist.co.kr/news/photo/202307/170226_100422_1733.jpg")!),
+                         board: Board(identifier: 1,
+                                      title: "최강 아이돌",
+                                      description: "게시글 설명",
+                                      thumbnailImageURL: nil,
+                                      videoURL: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8")!,
+                                      latitude: 127.060123123,
+                                      longitude: 37.0532156213),
+                         tag: ["아이브", "yujin"])
+
+        static let videoURLNilPost = Post(member: Member(identifier: 1,
+                                        username: "안유진",
+                                        introduce: "안녕하세요, 아이브의 안유진입니다.",
+                                        profileImageURL: URL(string: "https://cdn.footballist.co.kr/news/photo/202307/170226_100422_1733.jpg")!),
+                         board: Board(identifier: 1,
+                                      title: "최강 아이돌",
+                                      description: "게시글 설명",
+                                      thumbnailImageURL: nil,
+                                      videoURL: nil,
+                                      latitude: 127.060123123,
+                                      longitude: 37.0532156213),
+                         tag: ["아이브", "yujin"])
+
         static let post1 = Post(member: Member(identifier: 1,
                                         username: "안유진",
                                         introduce: "안녕하세요, 아이브의 안유진입니다.",
