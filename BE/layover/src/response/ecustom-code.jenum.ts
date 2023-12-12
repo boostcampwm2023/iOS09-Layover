de691a746fd0c2a0b99b5315eb9cf80da91012af
hw-ani <pcbmlh73@gmail.com>
2023-12-12 23:12:37 +0900

    :wrench: Refresh token 관련 응답 추가

--- BE/layover/src/response/ecustom-code.jenum.ts
+++ BE/layover/src/response/ecustom-code.jenum.ts
@@ -9,7 +9,7 @@ export const enum EMessage {
   TOKEN_GENERATE_ERR = 'token을 생성하는 과정에서 오류가 발생했습니다.',
   REFRESH_TOKEN_SAVE_ERR = 'refresh token을 저장하는 과정에서 오류가 발생했습니다.',
   SAVE_MEMBER_INFO_ERR = '회원정보를 저장하는 과정에서 오류가 발생했습니다.',
-  INVALID_IDENTITY_TOKEN = '유효하지 않은identity token입니다.',
+  INVALID_IDENTITY_TOKEN = '유효하지 않은 identity token입니다.',
   NOT_JWT_TYPE = '토큰이 JWT 형식이 아닙니다.',
   JWT_EXPIRED = '토큰 만료기간이 경과하였습니다.',
   INVALID_JWT = '유효하지 않은 JWT 토큰입니다.',
@@ -18,6 +18,7 @@ export const enum EMessage {
   NO_DATA_JWT = '토큰 데이터가 존재하지 않습니다.',
   NO_SOME_PAYLOAD_DATA_JWT = 'JWT payload에 필수 데이터가 모두 존재하지 않습니다.',
   ACCESS_TOKEN_NOT_EXPIRED = 'Access token 유효기간이 지나지 않았으므로 Refresh token을 이용한 재발급이 불가능합니다.',
+  INVALUD_REFRESH_TOKEN = '유효하지 않은 refresh token입니다.',
   JWT_ALGORITHM_ERR = '지원하지 않는 알고리즘으로 서명된 토큰입니다.',
   DUPLICATED_USERNAME = '중복 닉네임이 존재합니다.',
   INVALID_MEMBER_ID = '존재하지 않는 회원 id 입니다.',
@@ -64,6 +65,8 @@ export class ECustomCode extends EnumType<ECustomCode>() {
   static readonly NO_SOME_PAYLOAD_DATA_JWT = new ECustomCode(HttpStatus.BAD_REQUEST, EMessage.NO_SOME_PAYLOAD_DATA_JWT);
 
   static readonly ACCESS_TOKEN_NOT_EXPIRED = new ECustomCode(HttpStatus.BAD_REQUEST, EMessage.ACCESS_TOKEN_NOT_EXPIRED);
+
+  static readonly INVALUD_REFRESH_TOKEN = new ECustomCode(HttpStatus.BAD_REQUEST, EMessage.INVALUD_REFRESH_TOKEN);
 
   static readonly JWT_ALGORITHM_ERR = new ECustomCode(HttpStatus.BAD_REQUEST, EMessage.JWT_ALGORITHM_ERR);
 
