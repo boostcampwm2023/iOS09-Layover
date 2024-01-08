166e75680d655f153bedc13d136f63c308cff18c
chopmozzi <44396392+chopmozzi@users.noreply.github.com>
2024-01-09 00:39:21 +0900

    :white_check_mark: PlaybackInteractor Test 1차

--- iOS/Layover/Layover/Workers/Mocks/MockPlaybackWorker.swift
+++ iOS/Layover/Layover/Workers/Mocks/MockPlaybackWorker.swift
@@ -27,10 +27,15 @@ final class MockPlaybackWorker: PlaybackWorkerProtocol {
     }
 
     func makeInfiniteScroll(posts: [Post]) -> [Post] {
-        guard let tempLastVideo: Post = posts.last,
-              let tempFirstVideo: Post = posts.first
-        else { return posts }
-        var tempVideos: [Post] = posts
+        var tempVideos: [Post] = []
+        for post in posts {
+            if post.board.videoURL != nil, post.board.status == .complete {
+                tempVideos.append(post)
+            }
+        }
+        guard let tempLastVideo: Post = tempVideos.last,
+              let tempFirstVideo: Post = tempVideos.first
+        else { return tempVideos }
         tempVideos.insert(tempLastVideo, at: 0)
         tempVideos.append(tempFirstVideo)
         return tempVideos
